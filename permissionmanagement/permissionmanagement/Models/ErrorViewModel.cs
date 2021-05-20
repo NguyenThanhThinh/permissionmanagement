0e7c93d8d94424c827a4f5680bc673780b381d5d
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2021-05-20 12:26:47 +0900

    add

---
+++ permissionmanagement/permissionmanagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace permissionmanagement.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
