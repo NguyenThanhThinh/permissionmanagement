dd1bdc4d9b9abf5dc1e39a1aba1bd020f4a9ed67
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2021-05-20 15:57:23 +0900

    update

--- permissionmanagement/permissionmanagement/Controllers/RolesController.cs
+++ permissionmanagement/permissionmanagement/Controllers/RolesController.cs
@@ -7,27 +7,27 @@ using System.Threading.Tasks;
 namespace permissionmanagement.Controllers
 {
 	[Authorize(Roles = "SuperAdmin")]
-    public class RolesController : Controller
-    {
-        private readonly RoleManager<IdentityRole> _roleManager;
+	public class RolesController : Controller
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
 
-        public RolesController(RoleManager<IdentityRole> roleManager)
-        {
-            _roleManager = roleManager;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var roles = await _roleManager.Roles.ToListAsync();
-            return View(roles);
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddRole(string roleName)
-        {
-            if (roleName != null)
-            {
-                await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
-            }
-            return RedirectToAction("Index");
-        }
-    }
+		public RolesController(RoleManager<IdentityRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var roles = await _roleManager.Roles.ToListAsync();
+			return View(roles);
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddRole(string roleName)
+		{
+			if (roleName != null)
+			{
+				await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+			}
+			return RedirectToAction("Index");
+		}
+	}
 }
--- permissionmanagement/permissionmanagement/Controllers/UsersController.cs
+++ permissionmanagement/permissionmanagement/Controllers/UsersController.cs
@@ -8,18 +8,18 @@ using System.Threading.Tasks;
 namespace permissionmanagement.Controllers
 {
 	[Authorize]
-    public class UsersController : Controller
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        public UsersController(UserManager<IdentityUser> userManager)
-        {
-            _userManager = userManager;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
-            var allUsersExceptCurrentUser = await _userManager.Users.Where(a => a.Id != currentUser.Id).ToListAsync();
-            return View(allUsersExceptCurrentUser);
-        }
-    }
+	public class UsersController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		public UsersController(UserManager<IdentityUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+			var allUsersExceptCurrentUser = await _userManager.Users.Where(a => a.Id != currentUser.Id).ToListAsync();
+			return View(allUsersExceptCurrentUser);
+		}
+	}
 }
