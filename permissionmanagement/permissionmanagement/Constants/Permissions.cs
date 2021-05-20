ed6f427d47a1863694360574f26662e4b07c5444
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2021-05-20 12:30:28 +0900

    add permission

---
+++ permissionmanagement/permissionmanagement/Constants/Permissions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace permissionmanagement.Constants
+{
+	public static class Permissions
+	{
+		public static List<string> GeneratePermissions(string controller)
+		{
+			return new List<string>()
+		{
+			$"Permissions.{controller}.Create",
+			$"Permissions.{controller}.View",
+			$"Permissions.{controller}.Edit",
+			$"Permissions.{controller}.Delete",
+		};
+		}
+		public static class Products
+		{
+			public const string View = "Permissions.Products.View";
+			public const string Create = "Permissions.Products.Create";
+			public const string Edit = "Permissions.Products.Edit";
+			public const string Delete = "Permissions.Products.Delete";
+		}
+	}
+}
